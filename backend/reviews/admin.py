f2cf6721710794d6b714f1e9d97cdca4e6ef003e
Rémi <remi.barbe.work@gmail.com>
2026-08-20 16:17:04 +0200

    wip: register Actor, Movie, Review in django admin

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Actor, Movie, Review
 
 # Register your models here.
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name"]
+    search_fields = ["first_name", "last_name"]
+    
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    search_fields = ["title"]
+    filter_horizontal = ["actors"]
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["id", "movie", "grade"]
+    list_filter = ["grade"]
+    autocomplete_fields = ["movie"]
